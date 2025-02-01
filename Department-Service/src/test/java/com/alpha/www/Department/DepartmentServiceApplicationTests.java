d380b0d057855d8104d2fed16c870fad72bedf85
Nick <masihnishit@gmail.com>
2025-02-01 15:19:35 +0530

    new project's- employee & department

---
+++ Department-Service/src/test/java/com/alpha/www/Department/DepartmentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alpha.www.Department;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepartmentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
