3471639783a17ccddb3bd566264f7d5367a50372
Nick <masihnishit@gmail.com>
2025-02-02 12:34:53 +0530

    service-registry new project

---
+++ service-registry/src/test/java/com/alpha/www/serviceregistry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alpha.www.serviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
