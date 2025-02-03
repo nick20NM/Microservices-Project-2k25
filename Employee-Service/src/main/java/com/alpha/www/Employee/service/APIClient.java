e18a3437783cd853478ed4bc98bb4a7b1ce27bed
Nick <masihnishit@gmail.com>
2025-02-03 18:27:01 +0530

    Load Balancing with Eureka, Open Feign and Spring Cloud LoadBalancer

--- Employee-Service/src/main/java/com/alpha/www/Employee/service/APIClient.java
+++ Employee-Service/src/main/java/com/alpha/www/Employee/service/APIClient.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import com.alpha.www.Employee.dto.DepartmentDto;
 
-@FeignClient(url = "http://localhost:8080", value = "DEPARTMENT-SERVICE")
+@FeignClient(name = "DEPARTMENT-SERVICE")
 public interface APIClient {
 	@GetMapping("api/department/{department-code}")
 	DepartmentDto getDepartmentByCode(@PathVariable("department-code") String departmentCode);
